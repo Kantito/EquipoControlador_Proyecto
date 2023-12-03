96e7d14fa77619fb5bba5b2b079e06a66a7403c7
Mauricio Alpizar <m.alpizar@hotmail.com>
2023-12-03 11:36:17 -0600

    .

--- EquipoControlador_Proyecto/Opciones.cs
+++ EquipoControlador_Proyecto/Opciones.cs
@@ -24,7 +24,7 @@ namespace EquipoControlador_Proyecto
 
         private void Resolucion_pantalla_Click(object sender, EventArgs e)
         {
-
+            ejecutarComando("powershell -c \"(Get-WmiObject -Namespace root\cimv2 -Class Win32_VideoController).CurrentHorizontalResolution, (Get-WmiObject -Namespace root\cimv2 -Class Win32_VideoController).CurrentVerticalResolution");
         }
 
         private void Bajar_volumen_Click(object sender, EventArgs e)
@@ -35,8 +35,65 @@ namespace EquipoControlador_Proyecto
         private void Nombre_SO_Click(object sender, EventArgs e)
         {
             String mensaje = Environment.OSVersion.VersionString;
-            MessageBox.Show(mensaje, "Nombre Sistema Operativo", MessageBoxButtons.OK, 
+            MessageBox.Show(mensaje, "Nombre Sistema Operativo", MessageBoxButtons.OK,
                 MessageBoxIcon.Information);
+        }
+
+        private void Plataforma_SO_Click(object sender, EventArgs e)
+        {
+            String mensaje = Environment.OSVersion.Platform.ToString();
+            MessageBox.Show(mensaje, "Plataforma del Sistema Operativo", MessageBoxButtons.OK,
+            MessageBoxIcon.Information);
+            
+        }
+
+        private void Version_SO_Click(object sender, EventArgs e)
+        {
+            String mensaje = Environment.OSVersion.Version.ToString();
+            MessageBox.Show(mensaje, "Version del Sistema Operativo", MessageBoxButtons.OK,
+            MessageBoxIcon.Information);
+        }
+
+        private void Nombre_Equipo_Click(object sender, EventArgs e)
+        {
+            String mensaje = Environment.MachineName;
+            MessageBox.Show(mensaje, "Nombre del equipo", MessageBoxButtons.OK,
+            MessageBoxIcon.Information);
+        }
+
+        private void Info_Procesador(object sender, EventArgs e)
+        {
+            ejecutarComando("wmic cpu get caption, deviceid, name, numberofcores, maxclockspeed");
+        }
+
+        private void Total_RAM_Click(object sender, EventArgs e)
+        {
+            ejecutarComando("powershell -c \"Get-WmiObject Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum | Select-Object @{Name='TotalGB';Expression={$_.Sum / 1GB}}");
+        }
+
+        private void Nombre_Usuario_Click(object sender, EventArgs e)
+        {
+            WindowsIdentity identidad = WindowsIdentity.GetCurrent();
+            if (identidad != null)
+            {
+                MessageBox.Show(identidad.Name, "Nombre del usuario actual", MessageBoxButtons.OK,
+           MessageBoxIcon.Information);
+            }
+        }
+
+        private void ejecutarComando(string comando)
+        {
+            System.Diagnostics.ProcessStartInfo info = null;
+            System.Diagnostics.Process proceso = null;
+
+            info = new System.Diagnostics.ProcessStartInfo("cmd", "/c" + comando);
+            info.RedirectStandardOutput = true;
+            info.UseShellExecute = false;
+            info.CreateNoWindow = true;
+
+            proceso = new System.Diagnostics.Process();
+            proceso.StartInfo = info;
+            proceso.Start();
         }
     }
 }
