8a05294c1a364cbf4d8b079295c376cdaf6084cf
ByTeX17 <118131978+ByTeX17@users.noreply.github.com>
2023-12-02 23:00:00 -0600

    Add files via upload

---
+++ EquipoControlador_Proyecto/Opciones.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace EquipoControlador_Proyecto
+{
+    partial class Opciones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Nombre_SO = new Button();
+            Plataforma_SO = new Button();
+            Version_SO = new Button();
+            Nombre_Equipo = new Button();
+            Info_Procesador = new Button();
+            Total_RAM = new Button();
+            Lista_Unidades_Disco_Duro = new Button();
+            Resolucion_pantalla = new Button();
+            Nombre_Usuario = new Button();
+            Zona_horaria = new Button();
+            Fecha_Hora = new Button();
+            Procesos_ejecucion = new Button();
+            Captura_Pantalla = new Button();
+            Enviar_mensaje = new Button();
+            Subir_volumen = new Button();
+            Bajar_volumen = new Button();
+            Silenciar_sonido = new Button();
+            Apagar_equipo = new Button();
+            Reiniciar_equipo = new Button();
+            Cerrar_sesion_Windows = new Button();
+            Finalizar_Procesos = new Button();
+            Desconectar = new Button();
+            SuspendLayout();
+            // 
+            // Nombre_SO
+            // 
+            Nombre_SO.Location = new Point(49, 60);
+            Nombre_SO.Name = "Nombre_SO";
+            Nombre_SO.Size = new Size(144, 23);
+            Nombre_SO.TabIndex = 0;
+            Nombre_SO.Text = "Nombre SO";
+            Nombre_SO.UseVisualStyleBackColor = true;
+            Nombre_SO.Click += Nombre_SO_Click;
+            // 
+            // Plataforma_SO
+            // 
+            Plataforma_SO.Location = new Point(49, 119);
+            Plataforma_SO.Name = "Plataforma_SO";
+            Plataforma_SO.Size = new Size(144, 23);
+            Plataforma_SO.TabIndex = 1;
+            Plataforma_SO.Text = "Plataforma SO";
+            Plataforma_SO.UseVisualStyleBackColor = true;
+            // 
+            // Version_SO
+            // 
+            Version_SO.Location = new Point(49, 180);
+            Version_SO.Name = "Version_SO";
+            Version_SO.Size = new Size(144, 23);
+            Version_SO.TabIndex = 2;
+            Version_SO.Text = "Versión SO";
+            Version_SO.UseVisualStyleBackColor = true;
+            // 
+            // Nombre_Equipo
+            // 
+            Nombre_Equipo.Location = new Point(49, 243);
+            Nombre_Equipo.Name = "Nombre_Equipo";
+            Nombre_Equipo.Size = new Size(144, 23);
+            Nombre_Equipo.TabIndex = 3;
+            Nombre_Equipo.Text = "Nombre Equipo";
+            Nombre_Equipo.UseVisualStyleBackColor = true;
+            // 
+            // Info_Procesador
+            // 
+            Info_Procesador.Location = new Point(49, 303);
+            Info_Procesador.Name = "Info_Procesador";
+            Info_Procesador.Size = new Size(144, 23);
+            Info_Procesador.TabIndex = 4;
+            Info_Procesador.Text = "Información Procesador";
+            Info_Procesador.UseVisualStyleBackColor = true;
+            // 
+            // Total_RAM
+            // 
+            Total_RAM.Location = new Point(49, 363);
+            Total_RAM.Name = "Total_RAM";
+            Total_RAM.Size = new Size(144, 23);
+            Total_RAM.TabIndex = 5;
+            Total_RAM.Text = "Total RAM";
+            Total_RAM.UseVisualStyleBackColor = true;
+            // 
+            // Lista_Unidades_Disco_Duro
+            // 
+            Lista_Unidades_Disco_Duro.Location = new Point(234, 60);
+            Lista_Unidades_Disco_Duro.Name = "Lista_Unidades_Disco_Duro";
+            Lista_Unidades_Disco_Duro.Size = new Size(175, 23);
+            Lista_Unidades_Disco_Duro.TabIndex = 6;
+            Lista_Unidades_Disco_Duro.Text = "Lista Unidades de Disco Duro";
+            Lista_Unidades_Disco_Duro.UseVisualStyleBackColor = true;
+            // 
+            // Resolucion_pantalla
+            // 
+            Resolucion_pantalla.Location = new Point(234, 119);
+            Resolucion_pantalla.Name = "Resolucion_pantalla";
+            Resolucion_pantalla.Size = new Size(175, 23);
+            Resolucion_pantalla.TabIndex = 11;
+            Resolucion_pantalla.Text = "Resolución Pantalla";
+            Resolucion_pantalla.UseVisualStyleBackColor = true;
+            Resolucion_pantalla.Click += Resolucion_pantalla_Click;
+            // 
+            // Nombre_Usuario
+            // 
+            Nombre_Usuario.Location = new Point(234, 180);
+            Nombre_Usuario.Name = "Nombre_Usuario";
+            Nombre_Usuario.Size = new Size(175, 23);
+            Nombre_Usuario.TabIndex = 12;
+            Nombre_Usuario.Text = "Nombre Usuario";
+            Nombre_Usuario.UseVisualStyleBackColor = true;
+            // 
+            // Zona_horaria
+            // 
+            Zona_horaria.Location = new Point(234, 243);
+            Zona_horaria.Name = "Zona_horaria";
+            Zona_horaria.Size = new Size(175, 23);
+            Zona_horaria.TabIndex = 13;
+            Zona_horaria.Text = "Zona Horaria Sistema";
+            Zona_horaria.UseVisualStyleBackColor = true;
+            // 
+            // Fecha_Hora
+            // 
+            Fecha_Hora.Location = new Point(234, 303);
+            Fecha_Hora.Name = "Fecha_Hora";
+            Fecha_Hora.Size = new Size(175, 23);
+            Fecha_Hora.TabIndex = 14;
+            Fecha_Hora.Text = "Fecha y hora equipo";
+            Fecha_Hora.UseVisualStyleBackColor = true;
+            // 
+            // Procesos_ejecucion
+            // 
+            Procesos_ejecucion.Location = new Point(234, 363);
+            Procesos_ejecucion.Name = "Procesos_ejecucion";
+            Procesos_ejecucion.Size = new Size(175, 23);
+            Procesos_ejecucion.TabIndex = 15;
+            Procesos_ejecucion.Text = "Procesos en ejecución";
+            Procesos_ejecucion.UseVisualStyleBackColor = true;
+            // 
+            // Captura_Pantalla
+            // 
+            Captura_Pantalla.Location = new Point(446, 60);
+            Captura_Pantalla.Name = "Captura_Pantalla";
+            Captura_Pantalla.Size = new Size(161, 23);
+            Captura_Pantalla.TabIndex = 16;
+            Captura_Pantalla.Text = "Realizar captura de pantalla";
+            Captura_Pantalla.UseVisualStyleBackColor = true;
+            // 
+            // Enviar_mensaje
+            // 
+            Enviar_mensaje.Location = new Point(446, 119);
+            Enviar_mensaje.Name = "Enviar_mensaje";
+            Enviar_mensaje.Size = new Size(161, 23);
+            Enviar_mensaje.TabIndex = 17;
+            Enviar_mensaje.Text = "Enviar Mensaje";
+            Enviar_mensaje.UseVisualStyleBackColor = true;
+            // 
+            // Subir_volumen
+            // 
+            Subir_volumen.Location = new Point(446, 180);
+            Subir_volumen.Name = "Subir_volumen";
+            Subir_volumen.Size = new Size(161, 23);
+            Subir_volumen.TabIndex = 18;
+            Subir_volumen.Text = "Subir Volumen";
+            Subir_volumen.UseVisualStyleBackColor = true;
+            // 
+            // Bajar_volumen
+            // 
+            Bajar_volumen.Location = new Point(446, 243);
+            Bajar_volumen.Name = "Bajar_volumen";
+            Bajar_volumen.Size = new Size(161, 23);
+            Bajar_volumen.TabIndex = 19;
+            Bajar_volumen.Text = "Bajar Volumen";
+            Bajar_volumen.UseVisualStyleBackColor = true;
+            Bajar_volumen.Click += Bajar_volumen_Click;
+            // 
+            // Silenciar_sonido
+            // 
+            Silenciar_sonido.Location = new Point(446, 303);
+            Silenciar_sonido.Name = "Silenciar_sonido";
+            Silenciar_sonido.Size = new Size(161, 23);
+            Silenciar_sonido.TabIndex = 20;
+            Silenciar_sonido.Text = "Silenciar Sonido";
+            Silenciar_sonido.UseVisualStyleBackColor = true;
+            // 
+            // Apagar_equipo
+            // 
+            Apagar_equipo.Location = new Point(446, 363);
+            Apagar_equipo.Name = "Apagar_equipo";
+            Apagar_equipo.Size = new Size(161, 23);
+            Apagar_equipo.TabIndex = 21;
+            Apagar_equipo.Text = "Apagar Equipo";
+            Apagar_equipo.UseVisualStyleBackColor = true;
+            // 
+            // Reiniciar_equipo
+            // 
+            Reiniciar_equipo.Location = new Point(638, 60);
+            Reiniciar_equipo.Name = "Reiniciar_equipo";
+            Reiniciar_equipo.Size = new Size(109, 23);
+            Reiniciar_equipo.TabIndex = 22;
+            Reiniciar_equipo.Text = "Reiniciar Equipo";
+            Reiniciar_equipo.UseVisualStyleBackColor = true;
+            // 
+            // Cerrar_sesion_Windows
+            // 
+            Cerrar_sesion_Windows.Location = new Point(638, 119);
+            Cerrar_sesion_Windows.Name = "Cerrar_sesion_Windows";
+            Cerrar_sesion_Windows.Size = new Size(109, 23);
+            Cerrar_sesion_Windows.TabIndex = 23;
+            Cerrar_sesion_Windows.Text = "Cerrar Sesión";
+            Cerrar_sesion_Windows.UseVisualStyleBackColor = true;
+            // 
+            // Finalizar_Procesos
+            // 
+            Finalizar_Procesos.Location = new Point(638, 180);
+            Finalizar_Procesos.Name = "Finalizar_Procesos";
+            Finalizar_Procesos.Size = new Size(109, 23);
+            Finalizar_Procesos.TabIndex = 24;
+            Finalizar_Procesos.Text = "Finalizar Procesos";
+            Finalizar_Procesos.UseVisualStyleBackColor = true;
+            // 
+            // Desconectar
+            // 
+            Desconectar.Location = new Point(638, 243);
+            Desconectar.Name = "Desconectar";
+            Desconectar.Size = new Size(109, 23);
+            Desconectar.TabIndex = 25;
+            Desconectar.Text = "Desconectar";
+            Desconectar.UseVisualStyleBackColor = true;
+            Desconectar.Click += Desconectar_Click;
+            // 
+            // Opciones
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(Desconectar);
+            Controls.Add(Finalizar_Procesos);
+            Controls.Add(Cerrar_sesion_Windows);
+            Controls.Add(Reiniciar_equipo);
+            Controls.Add(Apagar_equipo);
+            Controls.Add(Silenciar_sonido);
+            Controls.Add(Bajar_volumen);
+            Controls.Add(Subir_volumen);
+            Controls.Add(Enviar_mensaje);
+            Controls.Add(Captura_Pantalla);
+            Controls.Add(Procesos_ejecucion);
+            Controls.Add(Fecha_Hora);
+            Controls.Add(Zona_horaria);
+            Controls.Add(Nombre_Usuario);
+            Controls.Add(Resolucion_pantalla);
+            Controls.Add(Lista_Unidades_Disco_Duro);
+            Controls.Add(Total_RAM);
+            Controls.Add(Info_Procesador);
+            Controls.Add(Nombre_Equipo);
+            Controls.Add(Version_SO);
+            Controls.Add(Plataforma_SO);
+            Controls.Add(Nombre_SO);
+            Name = "Opciones";
+            Text = "Opciones";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button Nombre_SO;
+        private Button Plataforma_SO;
+        private Button Version_SO;
+        private Button Nombre_Equipo;
+        private Button Info_Procesador;
+        private Button Total_RAM;
+        private Button Lista_Unidades_Disco_Duro;
+        private Button Resolucion_pantalla;
+        private Button Nombre_Usuario;
+        private Button Zona_horaria;
+        private Button Fecha_Hora;
+        private Button Procesos_ejecucion;
+        private Button Captura_Pantalla;
+        private Button Enviar_mensaje;
+        private Button Subir_volumen;
+        private Button Bajar_volumen;
+        private Button Silenciar_sonido;
+        private Button Apagar_equipo;
+        private Button Reiniciar_equipo;
+        private Button Cerrar_sesion_Windows;
+        private Button Finalizar_Procesos;
+        private Button Desconectar;
+    }
+}
---
+++ EquipoControlador_Proyecto/Opciones.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EquipoControlador_Proyecto
+{
+    public partial class Opciones : Form
+    {
+        public Opciones()
+        {
+            InitializeComponent();
+        }
+
+        private void Desconectar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Resolucion_pantalla_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Bajar_volumen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Nombre_SO_Click(object sender, EventArgs e)
+        {
+            String mensaje = Environment.OSVersion.VersionString;
+            MessageBox.Show(mensaje, "Nombre Sistema Operativo", MessageBoxButtons.OK, 
+                MessageBoxIcon.Information);
+        }
+    }
+}
